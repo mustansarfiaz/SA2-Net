6bdccd9f7fac42ff5786cb998acff38f8b2a1890
Mustansar Fiaz <mustansar3797@gmail.com>
2023-09-18 20:20:50 +0400

    Update test_model.py

--- MoNuSeg_GLaS/test_model.py
+++ MoNuSeg_GLaS/test_model.py
@@ -89,17 +89,13 @@ if __name__ == '__main__':
         if config.task_name is "GlaS":
             test_num = 80
             model_type = config.model_name
-            #model_path = "./GlaS/"+model_type+"/"+test_session+"/models/best_model-"+model_type+".pth.tar"
-            model_path = "./GlaS/"+model_type+"/"+test_session+"/models/fold_"+str(i+1)+"/best_model-"+model_type+".pth.tar"
+            model_path = "./GlaS/"+model_type+"/"+test_session+"/models/fold_"+str(i+1)+"/val_best_model-"+model_type+".pth.tar"
     
         elif config.task_name is "MoNuSeg":
             test_num = 14
             model_type = config.model_name
-            #model_path = "./MoNuSeg/"+model_type+"/"+test_session+"/models/best_model-"+model_type+".pth.tar"
-            model_path = "./MoNuSeg/"+model_type+"/"+test_session+"/models/fold_"+str(i+1)+"/test_best_model-"+model_type+".pth.tar"
-            #model_path = "./MoNuSeg/"+model_type+"/"+test_session+"/models/fold_"+str(i+1)+"/val_best_model-"+model_type+".pth.tar"
-            #model_path = "./MoNuSeg/"+model_type+"/"+test_session+"/models/fold_"+str(i+1)+"/model-"+model_type+"-last.pth.tar"
-    
+            model_path = "./MoNuSeg/"+model_type+"/"+test_session+"/models/fold_"+str(i+1)+"/val_best_model-"+model_type+".pth.tar"
+              
     
         save_path  = config.task_name +'/'+ model_type +'/' + test_session + '/'
         vis_path = "./" + config.task_name + '_visualize_test/'
